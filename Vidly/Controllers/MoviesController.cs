e38c3e2696e088a24e897eb0cfe571a5f547b406
Can Dincer <dincercan@gmail.com>
2017-11-04 13:29:04 +0300

    Finished Customers and Movies Controller added Cshtmls for them

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -11,33 +11,45 @@ namespace Vidly.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies
-        public ActionResult Index()
-        {
-            return View();
-        }
+     
 
 
         public ActionResult Random()
         {
-            var movie = new Movie() { Name = "Shrek" };
+            var movie = new Movie() { Name = "Shrek!" };
             var customers = new List<Customer>
-            {
-                new Customer {Name ="Customer 1" },
-                new Customer {Name="Customer 2" }
-
-            };
+             {
+                 new Customer { Name = "Customer 1" },
+                 new Customer { Name = "Customer 2" }
+             };
 
             var viewModel = new RandomMovieViewModel
             {
                 Movie = movie,
                 Customers = customers
-
             };
-
-
-
 
             return View(viewModel);
         }
+
+        public ViewResult Index()
+          {
+              var movies = GetMovies();
+  
+              return View(movies);    
+          }
+  
+          private IEnumerable<Movie> GetMovies()
+          {
+              return new List<Movie>
+              {
+                  new Movie { Id = 1, Name = "Shrek" },
+                  new Movie { Id = 2, Name = "Wall-e" }
+              };
+          }
+  
+
     }
+
+
 }
