3ba3251e8bb93a158c9ba9215b4cf1b08b7a7b41
Can Dincer <dincercan@gmail.com>
2017-11-04 12:17:14 +0300

    Setup CSS and added ViewModels did some stuff in random.cshtml

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+            var customers = new List<Customer>
+            {
+                new Customer {Name ="Customer 1" },
+                new Customer {Name="Customer 2" }
+
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+
+            };
+
+
+
+
+            return View(viewModel);
+        }
+    }
+}
