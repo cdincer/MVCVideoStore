6f476b4bc16330f336fe31e5a9490dc81aa17887
Can Dincer <dincercan@gmail.com>
2017-11-06 15:23:08 +0300

    Customer Details works.

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -31,7 +31,7 @@ using System.Data.Entity;
   
           public ActionResult Details(int id)
           {
-              var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+              var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
   
               if (customer == null)
                   return HttpNotFound();
