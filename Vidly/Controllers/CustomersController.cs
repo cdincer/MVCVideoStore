503b53f12b846b7f99ac2c3535875e04d7459354
Can Dincer <dincercan@gmail.com>
2017-12-22 15:54:31 +0300

    New page revised.

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -24,6 +24,7 @@ namespace Vidly.Controllers
             var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(), 
                 MembershipTypes = membershipTypes
 
             };
