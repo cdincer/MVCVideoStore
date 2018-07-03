87104cd0811b023ed8e9b0fad9a3a0db3ef67972
CanD <CanD@DESKTOP-ON8SQIF>
2018-07-03 20:58:39 +0300

    Revert "Movies Details/Edit Page added"
    This reverts commit 73be0539adbec6e5f4d1d671d95a622549a74dae.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -11,7 +11,9 @@ namespace Vidly.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies
+
         private ApplicationDbContext _context;
+
 
         public MoviesController()
         {
@@ -32,38 +34,8 @@ namespace Vidly.Controllers
             return View();
         }
 
-        [HttpPost]
-        public ActionResult Save(Movie EditedMovie)
-        {
-
-            if (!ModelState.IsValid)
-            {
-                var errors = ModelState.Select(x => x.Value.Errors)
-                        .Where(y => y.Count > 0)
-                        .ToList();
-
-                var viewModel = new Movie();
-              
-                return View("CustomerForm", viewModel);
-            }
 
 
-            if (EditedMovie.Id == 0)
-                _context.Movies.Add(EditedMovie);
-            else
-            {
-                var customerInDb = _context.Movies.Single(c => c.Id == EditedMovie.Id);
-
-                customerInDb.Name = EditedMovie.Name;
-                customerInDb.ReleaseDate = EditedMovie.ReleaseDate;
-                customerInDb.StockAmount = EditedMovie.StockAmount;
-                
-
-            }
-            _context.SaveChanges();
-
-            return RedirectToAction("Index", "Customers");
-        }
 
         [HttpPost]
         public ActionResult AddNewMovie(Movie ReceivedMovie)
@@ -78,14 +50,10 @@ namespace Vidly.Controllers
 
 
 
-        public ActionResult Details(Movie ReceivedMovie)
+        public ViewResult Details(Movie ReceivedMovie)
         {
             var foundmovie = _context.Movies.SingleOrDefault(c => c.Id == ReceivedMovie.Id);
-
-            if(foundmovie == null)
-                return HttpNotFound();
-
-            return View("Details", foundmovie);
+            return View(foundmovie);
         }
 
 
