e7336a28cd4ede7190218005253260a38bc33f71
cdincer <dincercan@gmail.com>
2018-07-15 20:39:40 +0300

    MigTrimv2

---
+++ VideoStore/Migrations/201807151738381_Trimv2.cs
@@ -0,0 +1,16 @@
+namespace VideoStore.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Trimv2 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
