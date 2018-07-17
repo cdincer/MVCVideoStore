ff02763642eefcae6768571c62c7c790e3abd82e
cdincer <dincercan@gmail.com>
2018-07-17 21:29:36 +0300

    Configuration Seeding v2

--- VideoStore/Migrations/Configuration.cs
+++ VideoStore/Migrations/Configuration.cs
@@ -17,13 +17,18 @@ namespace VideoStore.Migrations
         protected override void Seed(VideoStore.Models.ApplicationDbContext context)
         {
             //How to seed a table. Membership types seeind done through a migration. Its under "SeedingMembership"
-            context.Customers.AddOrUpdate(new Customer() { Name ="Can Two",IsSubscribeToNewsLetter=false,MembershipTypeId=1,BirthDate= new DateTime(1980, 1, 1) });
+            context.Customers.AddOrUpdate(new Customer() { Name = "Can One", IsSubscribeToNewsLetter = false, MembershipTypeId = 1, BirthDate = new DateTime(1979, 12, 12) });
+            context.Customers.AddOrUpdate(new Customer() { Name = "Can Two", IsSubscribeToNewsLetter = false, MembershipTypeId = 1, BirthDate = new DateTime(1980, 1, 1) });
             context.Customers.AddOrUpdate(new Customer() { Name = "Can Three", IsSubscribeToNewsLetter = true, MembershipTypeId = 2, BirthDate = new DateTime(1981, 2, 2) });
-            context.Customers.AddOrUpdate(new Customer() { Name = "Can Four", IsSubscribeToNewsLetter = true, MembershipTypeId = 3, BirthDate = new DateTime(1982, 1, 2) });
+            context.Customers.AddOrUpdate(new Customer() { Name = "Can Four", IsSubscribeToNewsLetter = true, MembershipTypeId = 3, BirthDate = new DateTime(1982, 3, 3) });
             context.Genres.AddOrUpdate(new Genre() { Id = 1, Name = "Genre 1" });
             context.Genres.AddOrUpdate(new Genre() { Id = 2, Name = "Genre 2" });
             context.Genres.AddOrUpdate(new Genre() { Id = 3, Name = "Genre 3" });
             context.Genres.AddOrUpdate(new Genre() { Id = 4, Name = "Genre 4" });
+            context.Movies.AddOrUpdate(new Movie() { Name = "Random Movie 1", StockAmount = 1, ReleaseDate = new DateTime(1979, 12, 12), GenreId = 1 });
+            context.Movies.AddOrUpdate(new Movie() { Name = "Random Movie 2", StockAmount = 2, ReleaseDate = new DateTime(1980, 1, 1), GenreId = 1 });
+            context.Movies.AddOrUpdate(new Movie() { Name = "Random Movie 3", StockAmount = 3, ReleaseDate = new DateTime(1981, 2, 2), GenreId = 1 });
+            context.Movies.AddOrUpdate(new Movie() { Name = "Random Movie 4", StockAmount = 4, ReleaseDate = new DateTime(1982, 3, 3), GenreId = 1 });
 
             context.SaveChanges();
 
