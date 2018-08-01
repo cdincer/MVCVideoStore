47eb0827f0baf52c5e7fdf5573485005b8eaf71d
cdincer <dincercan@gmail.com>
2018-08-01 20:42:45 +0300

    Api feeding for customers and movies

--- VideoStore/Controllers/CustomersController.cs
+++ VideoStore/Controllers/CustomersController.cs
@@ -79,9 +79,8 @@ namespace VideoStore.Controllers
         }
           public ViewResult Index()
           {
-              var customers = _context.Customers.Include(c => c.MembershipType).ToList();
   
-              return View(customers);
+              return View();
           }
   
           public ActionResult Details(int id)
--- VideoStore/Controllers/MoviesController.cs
+++ VideoStore/Controllers/MoviesController.cs
@@ -28,9 +28,8 @@ namespace VideoStore.Controllers
         }
         public ViewResult Index()
           {
-            var movies = _context.Movies.Include(m => m.Genre).ToList();
   
-              return View(movies);    
+              return View();    
           }
 
         public ViewResult AddNewMovie()
