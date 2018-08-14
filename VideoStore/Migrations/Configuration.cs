af78372fb1a6fc4e9f7f8f268284265aa9f0b01c
cdincer <dincercan@gmail.com>
2018-08-14 20:37:47 +0300

    User seeding with roles added Bootstrap Line:337 commented out

--- VideoStore/Migrations/Configuration.cs
+++ VideoStore/Migrations/Configuration.cs
@@ -1,6 +1,7 @@
 namespace VideoStore.Migrations
 {
     using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using Models;
     using System;
     using System.Collections.Generic;
@@ -25,7 +26,7 @@ namespace VideoStore.Migrations
         protected override void Seed(VideoStore.Models.ApplicationDbContext context)
         {
             string FirstName = "Customer";
-            //string NickPart
+            string NickPart = "Account";
             var rnd = new Random();
             //Random number generation through Linq. Random doesn't exclude duplicate.
             var randomNumbers = Enumerable.Range(1, 49).OrderBy(x => rnd.Next()).Take(18).ToList();
@@ -36,6 +37,7 @@ namespace VideoStore.Migrations
             context.Customers.AddOrUpdate(h => h.Id,new Customer() { Name = FirstName + " " + randomNumbers[2], IsSubscribeToNewsLetter = false, MembershipTypeId = 1, BirthDate = new DateTime(1980, 1, 1) });
             context.Customers.AddOrUpdate(h => h.Id,new Customer() { Name = FirstName + " " + randomNumbers[3], IsSubscribeToNewsLetter = true, MembershipTypeId = 2, BirthDate = new DateTime(1981, 2, 2) });
             context.Customers.AddOrUpdate(h => h.Id, new Customer() { Name = FirstName + " " + randomNumbers[4], IsSubscribeToNewsLetter = true, MembershipTypeId = 3, BirthDate = new DateTime(1982, 3, 3) });
+
             context.Genres.AddOrUpdate(new Genre() { Id = 1, Name = "Genre 1" });
             context.Genres.AddOrUpdate(new Genre() { Id = 2, Name = "Genre 2" });
             context.Genres.AddOrUpdate(new Genre() { Id = 3, Name = "Genre 3" });
@@ -48,7 +50,29 @@ namespace VideoStore.Migrations
             context.Movies.AddOrUpdate(new Movie() { Name = "Random Movie"+" " + randomNumbers[10], StockAmount = RandomStockNumbers[1], ReleaseDate = new DateTime(1980, 1, 1), GenreId = Convert.ToByte(RandomGenreNumbers[1]) });
             context.Movies.AddOrUpdate(new Movie() { Name = "Random Movie"+" " + randomNumbers[11], StockAmount = RandomStockNumbers[2], ReleaseDate = new DateTime(1981, 2, 2), GenreId = Convert.ToByte(RandomGenreNumbers[2]) });
             context.Movies.AddOrUpdate(new Movie() { Name = "Random Movie" + " " + randomNumbers[12], StockAmount = RandomStockNumbers[3], ReleaseDate = new DateTime(1982, 3, 3), GenreId = Convert.ToByte(RandomGenreNumbers[3]) });
-            //context.Users.AddOrUpdate(new ApplicationUser() {UserName= };
+
+
+            //Seeding roles in AspNetRoles Table
+            if (!context.Roles.Any(r => r.Name == "CanManageMovies"))
+            {
+                var store = new RoleStore<IdentityRole>(context);
+                var manager = new RoleManager<IdentityRole>(store);
+                var role = new IdentityRole { Name = "CanManageMovies" };
+
+                manager.Create(role);
+            }
+            
+            //Seeding users in AspNetUsers Table
+            if (!context.Users.Any(u => u.UserName == "admin@videostore.com"))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+                var user = new ApplicationUser { UserName = "admin@videostore.com" ,Email= "admin@videostore.com" };
+
+                manager.Create(user, "Candincer1!");
+                manager.AddToRole(user.Id, "CanManageMovies");
+            }
+
             context.SaveChanges();
 
         }
