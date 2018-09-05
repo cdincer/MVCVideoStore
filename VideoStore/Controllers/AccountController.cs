db2275945ad57f1d190e15300998598c30b3d615
Can Dincer <dincercan@gmail.com>
2018-09-05 20:45:00 +0300

    AccountManagement Seperation

--- VideoStore/Controllers/AccountController.cs
+++ VideoStore/Controllers/AccountController.cs
@@ -482,18 +482,7 @@ namespace VideoStore.Controllers
             }
         }
 
-        public ApplicationDbContext _context2;
 
-        public ActionResult Index()
-        {
-            _context2 = new ApplicationDbContext();
-
-
-            var Users = _context2.Users.ToList();
-
-
-            return View(Users);
-        }
         #endregion
     }
 }
---
+++ VideoStore/Controllers/AccountManagementController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideoStore.Models;
+
+namespace VideoStore.Controllers
+{
+    public class AccountManagementController : Controller
+    {
+        // GET: AccountManagement
+        public ApplicationDbContext _context2;
+
+        public ActionResult Index()
+        {
+            _context2 = new ApplicationDbContext();
+
+
+            var Users = _context2.Users.ToList();
+
+
+            return View(Users);
+        }
+    }
+}
