b013f84a3c2d14efe6b92b26c2f5468c261f194a
Can Dincer <dincercan@gmail.com>
2018-09-11 19:28:54 +0300

    AMLViewModel implemented for accessing DB via Raw SQL

---
+++ VideoStore/ViewModels/AMLViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VideoStore.ViewModels
+{
+    public class AMLViewModel
+    {
+        public string Email {get; set;}
+        public string UserName{get; set;}
+        public string RoleName{get; set;}
+        public string UserId{get; set;}
+
+
+    }
+}
