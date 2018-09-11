7555503096207613d232805d1b9e87cb2578f9c8
Can Dincer <dincercan@gmail.com>
2018-09-11 19:29:44 +0300

    Redundant comment clean up

--- VideoStore/Controllers/AccountManagementController.cs
+++ VideoStore/Controllers/AccountManagementController.cs
@@ -17,15 +17,11 @@ namespace VideoStore.Controllers
         {
             _context2 = new ApplicationDbContext();
 
-            //Query For Getting 
-            //3 Table Join
-            //select utable.Email,utable.UserName,nroles.Name AS "Permission Level" ,uroles.UserId from AspNetUserRoles uroles LEFT JOIN AspNetRoles nroles ON nroles.Id = uroles.RoleId
-            //LEFT JOIN AspNetUsers utable ON utable.Id = uroles.UserId
+
 
           var UserRoles = _context2.Database.SqlQuery<AMLViewModel>("select utable.Email,utable.UserName,nroles.Name AS RoleName ,uroles.UserId from AspNetUserRoles uroles LEFT JOIN AspNetRoles nroles ON nroles.Id = uroles.RoleId LEFT JOIN AspNetUsers utable ON utable.Id = uroles.UserId");
-            var Conversion = UserRoles.ToList();
-            //var Roles = _context2.Roles.ToList();
-            //var Users = _context2.Users.ToList();
+           var Conversion = UserRoles.ToList();
+     
 
 
             return View(Conversion);
