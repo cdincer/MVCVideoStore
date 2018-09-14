344ef8fc6665aaa4da1bee9167e72619bd9c5cc2
Can Dincer <dincercan@gmail.com>
2018-09-14 22:04:18 +0300

    Rental Listing view and action added

--- VideoStore/Controllers/RentalsController.cs
+++ VideoStore/Controllers/RentalsController.cs
@@ -3,18 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VideoStore.Models;
 
 namespace VideoStore.Controllers
 {
     public class RentalsController : Controller
     {
+        public ApplicationDbContext _context;
+
         // GET: Rentals
         public ActionResult New()
         {
             return View();
         }
 
+        public ActionResult List()
+        {
+            _context = new ApplicationDbContext();
+            var Rentals = _context.Rentals.ToList();
 
+            return View(Rentals);
+        }
 
     }
 }
