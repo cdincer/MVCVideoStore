b30296251e210a26c7a443f1367904405b77526a
Can Dincer <dincercan@gmail.com>
2019-02-08 23:34:10 +0300

    JS folder adjustment

--- VideoStore/App_Start/BundleConfig.cs
+++ VideoStore/App_Start/BundleConfig.cs
@@ -22,10 +22,10 @@ namespace VideoStore
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/bootbox.js",
-                      "~/Scripts/respond.js",
+                      "~/Scripts/thirdparty/respond.js",
                       "~/Scripts/DataTables/jquery.datatables.js",
-                       "~/Scripts/typeahead.bundle.js",
-                       "~/Scripts/toastr.js"));
+                       "~/Scripts/thirdparty/typeahead.bundle.js",
+                       "~/Scripts/thirdparty/toastr.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap--lumen.css",
